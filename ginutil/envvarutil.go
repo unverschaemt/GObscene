153a0fe2e32b423af6ad8ad425c09f7f58347442
Simon Korz <zeit.vertreib@web.de>
2015-05-30 02:59:56 +0200

    Cleaned up envvarutil

--- ginutil/envvarutil.go
+++ ginutil/envvarutil.go
@@ -5,19 +5,10 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-func SetDB(db *mgo.Database) {
-
-}
-
 func GetDB(c *gin.Context) *mgo.Database {
 	db := c.MustGet("db")
 	return db.(*mgo.Database)
 }
-
-//
-//func GetSession(c  *gin.Context) *sessions.Session {
-//	return c.Get("Session").(*sessions.Session)
-//}
 
 func SetEnvvar(key string, value interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
